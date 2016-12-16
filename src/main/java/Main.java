ff70650f32b96bafd3d685d0ee1f04469a42cb45
zoolsher <zoolsher@gmail.com>
2016-12-16 17:28:26 +0800

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zoolsher on 2016/12/16.
+ * @author zoolsher
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
