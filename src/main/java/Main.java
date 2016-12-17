bd83dda7c7540e6b87784ecd448bb6f243af8d86
sumy <sunmingjian8@gmail.com>
2016-12-17 16:36:53 +0800

    add spring context

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,22 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
- * Created by zoolsher on 2016/12/16.
+ * Server Main Class.
+ *
  * @author zoolsher
+ * @author Sumy
  */
 public class Main {
 
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        logger.info("Server Start...");
+        logger.info("Loading Spring Context");
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("com.safecode.andcloud.configuration");
+        logger.info("Server initialization finished.");
     }
 
 }
---
+++ src/main/java/com/safecode/andcloud/configuration/SpringAppContext.java
@@ -0,0 +1,17 @@
+package com.safecode.andcloud.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Context configuration class.
+ *
+ * @author Sumy
+ */
+@Configuration
+public class SpringAppContext {
+
+    private final static Logger logger = LoggerFactory.getLogger(SpringAppContext.class);
+
+}
